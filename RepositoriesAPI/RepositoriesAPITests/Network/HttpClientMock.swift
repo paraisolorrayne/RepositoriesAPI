8913b85288a868d7ec9c8746577c54c992963859
lorrayneparaiso <lorrayne@candletech.com.br>
2020-05-29 18:31:35 -0300

    Add network tests

---
+++ RepositoriesAPI/RepositoriesAPITests/Network/HttpClientMock.swift
@@ -0,0 +1,39 @@
+//
+//  HttpClientMock.swift
+//  RepositoriesAPITests
+//
+//  Created by Lorrayne Paraiso on 29/05/20.
+//
+
+import XCTest
+@testable import RepositoriesAPI
+
+class ClientMock: ClientProtocol {
+    var isFailure = false
+    
+    func requestData<T : Decodable>(with setup: ClientSetup, completion: @escaping (Result<T>) -> Void) {
+        if isFailure {
+            completion(.failure(ClientError.invalidHttpResponse))
+            return
+        } else {
+            completion(generateData())
+        }
+    }
+    
+    private func generateData<T: Decodable>() -> Result<T> {
+        guard
+            let filePath = Bundle.main.path(forResource: "upcoming", ofType: "json")
+            else {
+                XCTFail("Could not mock data!")
+                return .failure(ClientError.brokenData)
+        }
+        do {
+            let jsonData = try Data(contentsOf: URL(fileURLWithPath: filePath),
+                                    options: .mappedIfSafe)
+            let decoder = JSONDecoder()
+            let responseModel = try decoder.decode(T.self, from: jsonData)
+            return .success(responseModel)
+        } catch { }
+        return .failure(ClientError.brokenData)
+    }
+}
