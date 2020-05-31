750d5be775a521b6622a43999ced4f780c596915
lorrayneparaiso <lorrayne@candletech.com.br>
2020-05-31 15:49:19 -0300

    Add fastlane config

--- RepositoriesAPI/RepositoriesAPIUITests/RepositoriesAPIUITests.swift
+++ RepositoriesAPI/RepositoriesAPIUITests/RepositoriesAPIUITests.swift
@@ -22,11 +22,12 @@ class RepositoriesAPIUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func renderHomeScreen() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        setupSnapshot(app, waitForAnimations: false)
+        snapshot("HomeScreen")
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
